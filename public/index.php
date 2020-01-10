19f1ead73a108b87a3dd2ef070860de1001c1028
Masaki Kawabata Neto <kawabata@usp.br>
2020-01-10 10:11:44 -0300

    implementando cache

--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@ require_once '../app/bootstrap.php';
 
 // Controller padrão
 $ws = new Uspdev\Evasao\Ws;
+# Instanciando biblioteca de cache
+use Uspdev\Cache\Cache;
+$c = new Cache($ws);
 
 // na raiz vamos colocar a documentação
 Flight::route('/', function () use ($ws) {
@@ -13,8 +16,10 @@ Flight::route('/', function () use ($ws) {
 });
 
 // vamos mapear todas as rotas para o controller padrão
-Flight::route('GET /@metodo:[a-z]+(/@param1)', function ($metodo, $param1) use ($ws) {
-    $out = $ws->$metodo($param1);
+Flight::route('GET /@metodo:[a-z]+(/@param1)', function ($metodo, $param1) use ($c) {
+    
+    $out = $c->getCached($metodo, [$param1]);
+
     $f = Flight::request()->query['format'];
     switch ($f) {
         case 'csv':
