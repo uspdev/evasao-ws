46d354a64665ae41edcd674604c8d64cdbabefdf
Masaki Kawabata Neto <kawabata@usp.br>
2020-01-10 11:47:27 -0300

    documentando as rotas

--- public/index.php
+++ public/index.php
@@ -4,20 +4,35 @@ require_once '../app/bootstrap.php';
 
 // Controller padrão
 $ws = new Uspdev\Evasao\Ws;
+
 # Instanciando biblioteca de cache
-use Uspdev\Cache\Cache;
-$c = new Cache($ws);
+$c = new Uspdev\Cache\Cache($ws);
 
 // na raiz vamos colocar a documentação
 Flight::route('/', function () use ($ws) {
-    $api['posgraduacao_url'] = getenv('DOMINIO') . '/posgraduacao';
-    $api['metodos'] = get_class_methods($ws);
+
+    $metodos = get_class_methods($ws);
+
+    foreach ($metodos as $m) {
+        // para cada método vamos obter os parâmetros
+        $r = new ReflectionMethod($ws, $m);
+        $params = $r->getParameters();
+        $p = '/';
+        foreach ($params as $param) {
+            $p .= '{' . $param->getName() . '}, ';
+        }
+        $p = substr($p,0,-2);
+        
+        // vamos apresentar na forma de url
+        $api[$m] = getenv('DOMINIO') . '/' . $m . $p;
+    }
     Flight::jsonf($api);
 });
 
+define('USPDEV_CACHE_DISABLE', true);
 // vamos mapear todas as rotas para o controller padrão
 Flight::route('GET /@metodo:[a-z]+(/@param1)', function ($metodo, $param1) use ($c) {
-    
+
     $out = $c->getCached($metodo, [$param1]);
 
     $f = Flight::request()->query['format'];
