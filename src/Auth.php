7e7ba0c4728bd47cb6e68498c21b14b10067569f
Masaki Kawabata Neto <kawabata@usp.br>
2020-02-18 10:06:38 -0300

    refatoracao grande

--- src/Auth.php
+++ src/Auth.php
@@ -4,100 +4,111 @@ namespace Uspdev\Evasao;
 
 class Auth
 {
-    public static function auth()
+    public $msg;
+
+    // formato: ['username' => pwd_hash];
+    private $users = [];
+
+    public function __construct($pwdfile = '')
     {
-        global $users;
-
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-        header('Access-Control-Allow-Headers: authorization');
-
-        if (!isset($_SERVER['PHP_AUTH_USER'])) {
-            \Flight::requireAuth();
-            exit;
+        if (empty($pwdfile)) {
+            $pwdfile = PWD_FILE;
         }
 
-        if (!isset($users[$_SERVER['PHP_AUTH_USER']]) or $users[$_SERVER['PHP_AUTH_USER']] != md5($_SERVER['PHP_AUTH_PW'])) {
-            \Flight::requireAuth();
-            exit;
+        // vamos ler o arquivo de senhas
+        if (($handle = fopen($pwdfile, 'r')) !== false) {
+            while (($user = fgetcsv($handle, 1000, ':')) !== false) {
+                $this->users[$user[0]] = $user[1];
+            }
+            fclose($handle);
         }
     }
 
-    public static function login()
+    public function auth()
     {
-        global $users;
 
+        // header('Access-Control-Allow-Origin: *');
+        // header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+        // header('Access-Control-Allow-Headers: authorization');
+
+        if (!isset($_SERVER['PHP_AUTH_USER'])) {
+            return false;
+        }
+
+        $user = $_SERVER['PHP_AUTH_USER'];
+        $pwd = $_SERVER['PHP_AUTH_PW'];
+
+        //echo $user,$pwd;exit;
+
+        // se o usuario não existir ou se a senha não conferir vamos negar acesso
+        if (!isset($this->users[$user]) or !password_verify($pwd, $this->users[$user])) {
+            return false;
+        }
+
+        // tudo certo, acesso liberado
+        return true;
+    }
+
+    public function login()
+    {
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
         header('Access-Control-Allow-Headers: authorization');
 
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
-            echo 'OK';
-            exit;
+            $this->msg = 'OK';
+            return true;
         }
 
         if (!isset($_SERVER['PHP_AUTH_USER'])) {
-            header('HTTP/1.0 401 Unauthorized');
             header('WWW-Authenticate: Basic realm="use this hash key to encode"');
-            echo 'Você deve digitar um login e senha válidos para acessar este recurso\n';
-            exit;
+            //header('HTTP/1.0 401 Unauthorized');
+            $this->msg = 'Você deve digitar um login e senha válidos para acessar este recurso';
+            return false;
         }
 
-        if (!isset($users[$_SERVER['PHP_AUTH_USER']]) or $users[$_SERVER['PHP_AUTH_USER']] != md5($_SERVER['PHP_AUTH_PW'])) {
-            header('HTTP/1.0 401 Unauthorized');
-            echo 'Credenciais inválidas';
-            exit();
+        if ($this->auth()) {
+            $this->msg = 'Login com successo';
+            return true;
         }
 
-        echo 'Login success';
-        exit;
+        $this->msg = 'Usuário ou senha inválidos';
+        return false;
     }
 
     public static function logout()
     {
-        header('HTTP/1.0 401 Unauthorized');
         header('WWW-Authenticate: Basic realm="use this hash key to encode"');
-        die('logout');
+        //header('HTTP/1.0 401 Unauthorized');
+        //exit;
+        //die('logout');
     }
 
-    // Implementa controle de acesso por IP
-    // Se definido IP_ACCESS_LIST, usará essa lista de ip/prefixo para
-    // autorizar o acesso a todos os endpoints
-    // Para desativar não defina a constante IP_ACCESS_LIST
-    public static function ipControl()
+    // Controle de acesso por IP
+    public static function ipControl($ipfile)
     {
-        define('IP_ACCESS_LIST', array(
-            ['143.107.233.0', '24'], // SET
-            ['143.107.181.0', '24'], // Campus 2: SAA, SEA
-            ['143.107.182.0', '24'], // STI
-            ['143.107.234.0', '24'], // SGS, STT
-            ['143.107.235.0', '24'], // SEL
-            ['143.107.238.0', '24'], // ALMOX, SEP
-            ['143.107.239.0', '24'], // SEM
-            ['10.233.0.0', '16'], // SET Intranet
-        ));
-
-        // se a lista for vazia ninguém tem acesso
-        //define('IP_ACCESS_LIST', []);
-
-        if (getenv('IP_CONTROL') === false) {
-            return true;
-        }
-
-        // vamos verificar se o IP está na lista
-        if (defined('IP_ACCESS_LIST')) {
-            foreach (IP_ACCESS_LIST as $ip_list) {
+        // vamos ler o arquivo de endereços autorizados
+        if (($handle = fopen($ipfile, 'r')) !== false) {
+            while (($row = fgetcsv($handle, 1000, ',')) !== false) {
+                
+                // e ver se o ip está na lista
                 // https://stackoverflow.com/questions/2869893/block-specific-ip-block-from-my-website-in-php
-                $network = ip2long($ip_list[0]);
-                $prefix = $ip_list[1];
+                $network = ip2long($row[0]);
+                $prefix = (int) $row[1];
                 $ip = ip2long($_SERVER['REMOTE_ADDR']);
 
                 if ($network >> (32 - $prefix) == $ip >> (32 - $prefix)) {
+                    // Se sim, vamos liberar o acesso
+                    fclose($handle);
                     return true;
                 }
             }
-        } 
-        // não houve match na lista então vamos negar acesso
-        return false;
+            fclose($handle);
+            // aqui vamos negar o acesso
+            return false;
+        } else {
+            echo 'Erro ao ler arquivo '.$ipfile;
+            exit;
+        }
     }
 }
